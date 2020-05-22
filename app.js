ffa8c6f965b6d095367f35c533534656d388eb3c
Garrett Trott <garretttrott@gmail.com>
2020-05-21 23:21:22 -0700

    add pud and views

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/hello", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
