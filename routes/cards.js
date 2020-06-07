bd72716ce334e60085eb69451e2794118ad1d5fe
Garrett Trott <garretttrott@gmail.com>
2020-06-06 23:42:13 -0700

    add public

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { data } = require("../data/flashcardData.json");
+const { cards } = data;
 
 router.get("/", (req, res) => {
-  res.render("card", {
-    prompt: "Who is buried in Grant's tomb?",
-  });
+  const numberOfCards = cards.length;
+  const flashcardID = Math.floor(Math.random() * numberOfCards);
+  res.redirect(`/cards/${flashcardID}`);
+});
+
+router.get("/:id", (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+
+  if (!side) {
+    return res.redirect(`/cards/${id}?side=question`);
+  }
+
+  const name = req.cookies.username;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+
+  const templateData = { id, text, name };
+
+  if (side === "question") {
+    templateData.hint = hint;
+    templateData.sideToShow = "answer";
+    templateData.sideToShowDisplay = "Answer";
+  } else if (side === "answer") {
+    templateData.sideToShow = "question";
+    templateData.sideToShowDisplay = "Question";
+  }
+  res.render("card", templateData);
 });
 
 module.exports = router;
